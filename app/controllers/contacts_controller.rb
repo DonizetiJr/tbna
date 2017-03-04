d592498e25340e0553838b3d187b4061e7e1d99b
DonizetiJr <dcs.junior22@gmail.com>
2017-03-03 22:59:55 -0300

    Backend of Emails sent

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -4,10 +4,15 @@ class ContactsController < ApplicationController
   end
 
   def create
+    sleep 1
     @contact = Contact.new(params[:contact])
     @contact.request = request
     if @contact.deliver
       flash.now[:error] = nil
+      respond_to do |format|
+        format.html { redirect_to root_path }
+        format.js # render contacts/create.js.erb
+      end
     else
       flash.now[:error] = 'Cannot send message.'
       render :new
